757d2e115689af9e113ce5e629da5c414111936f
deep <dp.rahman@gmail.com>
2018-06-18 13:22:54 +0600

    Completed

--- check_urls.php
+++ check_urls.php
@@ -23,7 +23,48 @@
             $port = (isset($url_pieces['port'])) ? $url_pieces['port'] : 80;
 
             // Connect using fsockopen:
-            
+            if($fp = fsockopen($url_pieces['host'], $port, $errno, $errstr, 30)){
+
+                // Send some data
+                $send = "HEAD $path HTTP/1.1\r\n";
+                $send .= "HOST:{$url_pieces['host']}\r\n";
+                $send .= "CONNECTION: close\r\n\r\n";
+                fwrite($fp,$send);
+                // Read the response
+                $data = fgets($fp,128);
+
+                // Close the connection
+                fclose($fp);
+
+                // Return the response code
+                list($response, $code) = explode(' ',$data);
+
+                if ($code == 200) {
+                    return array($code, 'good');
+                }else{
+                    return array($code, 'bad');
+                }
+            }else{ // No connection, return the error message:
+                return array($errstr, 'bad');
+            }
+        } // End of chk_url function
+
+        // Create the list of URLs:
+        $urls = array(
+            'http://www.larryullman.com/',
+            'http://www.larryullman.com/wp-admin/',
+            'http://www.yiiframework.com/tutorials/'
+        );
+
+        // Print the header
+        echo "<h2>Validating URLs</h2>";
+
+        // Kill the PHP time limit
+        set_time_limit(0);
+
+        foreach($urls as $url){
+            list($code, $class) = check_url($url);
+            echo "<p><a href=\"$url\" target=\"_new\">$url</a>(<span class=\"$class\">$code</span>)</p>\n";
         }
     ?>
 </body>
